1e04c7d74b9792b439a1bbd641e3427ebfcff3b1
Berkant AYAR <berkant.ayar@glomil.com>
2022-08-24 15:33:40 +0300

    x

---
+++ MyBlogApp.Shared/Utilities/Results/Concrete/Result.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyBlogApp.Shared.Utilities.Results.Abstract;
+using MyBlogApp.Shared.Utilities.Results.ComplexTypes;
+
+namespace MyBlogApp.Shared.Utilities.Results.Concrete
+{
+    public class Result:IResult
+    {
+        public Result(ResultStatus resultStatus)
+        {
+            ResultStatus = resultStatus;
+        }
+
+        public Result(ResultStatus resultStatus, string message)
+        {
+            ResultStatus = resultStatus;
+            Message = message;
+        }
+        public ResultStatus ResultStatus { get; }
+        public string Message { get; }
+        public Exception Exception { get; }
+    }
+}

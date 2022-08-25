275fc808114430853b67640499d62ad4f0598b20
Berkant AYAR <berkant.ayar@glomil.com>
2022-08-25 10:39:55 +0300

    Result,DataResult

--- MyBlogApp.Shared/Utilities/Results/Concrete/DataResult.cs
+++ MyBlogApp.Shared/Utilities/Results/Concrete/DataResult.cs
@@ -8,21 +8,31 @@ using MyBlogApp.Shared.Utilities.Results.ComplexTypes;
 
 namespace MyBlogApp.Shared.Utilities.Results.Concrete
 {
-    public class Result:IResult
+    public class DataResult<T>:IDataResult<T>
     {
-        public Result(ResultStatus resultStatus)
+        public DataResult(ResultStatus resultStatus,T data)
         {
             ResultStatus = resultStatus;
+            Data = data;
         }
-
-        public Result(ResultStatus resultStatus, string message, Exception exception)
+        public DataResult(ResultStatus resultStatus,string message, T data)
         {
             ResultStatus = resultStatus;
             Message = message;
+            Data = data;
+
+        }
+        public DataResult(ResultStatus resultStatus, string message, T data, Exception exception)
+        {
+            ResultStatus = resultStatus;
+            Message = message;
+            Data = data;
             Exception = exception;
         }
+
         public ResultStatus ResultStatus { get; }
         public string Message { get; }
         public Exception Exception { get; }
+        public T Data { get; }
     }
 }
